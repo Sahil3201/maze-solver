a7d59ce6069e29445413cfe4bdaac1287a955151
Sahil <lunawatsahil@gmail.com>
2024-03-03 19:28:34 +0000

    Added requirements.txt and updated readme, code

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ import copy, time
 import psutil
 import matplotlib.pyplot as plt
 
+MDP_ALGOS = [MdpValueIteration.value_iteration, MdpPolicyIteration.policy_iteration]
+SEARCH_ALGOS = [bfs.bfs, dfs.dfs, astar.a_star]
+ALL_ALGOS = SEARCH_ALGOS + MDP_ALGOS
+
 def memory_usage(func):
     def wrapper(*args, **kwargs):
         process = psutil.Process()
@@ -25,15 +29,15 @@ def plot_metrics(metric_names, metric_data, maze_size):
 
         plt.xlabel('Maze Size')
         plt.ylabel(metric_name)
-        plt.title(f'{metric_name} Comparison for Algorithms')
+        plt.title(f'{metric_name} vs maze size')
         plt.legend()
         plt.show()
 
 def compare_search_algos(maze_sizes_search_algo):
     # Lists to store metrics
-    time_taken_data = {'bfs': [], 'dfs': [], 'a_star': []}
-    cells_visited_data = {'bfs': [], 'dfs': [], 'a_star': []}
-    memory_used_data = {'bfs': [], 'dfs': [], 'a_star': []}
+    time_taken_data = {algo.__name__: [] for algo in SEARCH_ALGOS}
+    cells_visited_data = {algo.__name__: [] for algo in SEARCH_ALGOS}
+    memory_used_data = {algo.__name__: [] for algo in SEARCH_ALGOS}
 
 
     @memory_usage
@@ -44,7 +48,7 @@ def compare_search_algos(maze_sizes_search_algo):
         print(f"Maze size: {i}")
         input_maze = maze_generator.prims_algo(w=i[0], h=i[1])
         
-        for algo in [bfs.bfs, dfs.dfs, astar.a_star]:
+        for algo in SEARCH_ALGOS:
             maze = copy.deepcopy(input_maze)
             start, end = maze.set_start_end_points()
             args = [maze, i[0], i[1], start, end]
@@ -59,7 +63,7 @@ def compare_search_algos(maze_sizes_search_algo):
             cells_visited_data[algo.__name__].append(cells_visited)
             memory_used_data[algo.__name__].append(memory_used)
 
-    metric_names = ['Time Taken', 'Cells Visited', 'Memory Used']
+    metric_names = ['Time Taken for pathfinding', 'Cells Visited', 'Memory Used']
     metric_data = [time_taken_data, cells_visited_data, memory_used_data]
     plot_metrics(metric_names, metric_data, maze_sizes_search_algo)
 
@@ -67,15 +71,15 @@ def compare_search_algos(maze_sizes_search_algo):
     print("\nResults Table:")
     print(f"{'Maze Size':<15}{'Algorithm':<15}{'Time Taken':<15}{'Cells Visited':<15}{'Memory Used':<15}")
     for size in maze_sizes_search_algo:
-        for algo in [bfs.bfs, dfs.dfs, astar.a_star]:
+        for algo in SEARCH_ALGOS:
             print(f"{str(size):<15}{algo.__name__:<15}{time_taken_data[algo.__name__].pop(0):<15.5f}"
                 f"{cells_visited_data[algo.__name__].pop(0):<15}{memory_used_data[algo.__name__].pop(0):<15.2f}")
 
 def compare_mdp_algos(maze_sizes_mdp_algo):
     # Lists to store metrics
-    time_taken_data = {'value_iteration': [], 'policy_iteration': []}
-    iterations_data = {'value_iteration': [], 'policy_iteration': []}
-    memory_used_data = {'value_iteration': [], 'policy_iteration': []}
+    time_taken_data = {algo.__name__: [] for algo in MDP_ALGOS}
+    iterations_data = {algo.__name__: [] for algo in MDP_ALGOS}
+    memory_used_data = {algo.__name__: [] for algo in MDP_ALGOS}
 
 
     @memory_usage
@@ -86,7 +90,7 @@ def compare_mdp_algos(maze_sizes_mdp_algo):
         print(f"Maze size: {i}")
         input_maze = mdp_maze_generator.MdpMaze(w=i[0], h=i[1])
         
-        for algo in [MdpValueIteration.value_iteration, MdpPolicyIteration.policy_iteration]:
+        for algo in MDP_ALGOS:
             maze = copy.deepcopy(input_maze)
             start, end = maze.set_start_end_points()
             start.reward = -100
@@ -104,7 +108,7 @@ def compare_mdp_algos(maze_sizes_mdp_algo):
             iterations_data[algo.__name__].append(iterations)
             memory_used_data[algo.__name__].append(memory_used)
 
-    metric_names = ['Time Taken', 'Iterations Needed', 'Memory Used']
+    metric_names = ['Time Taken for convergence', 'Iterations Needed for convergence', 'Memory Used']
     metric_data = [time_taken_data, iterations_data, memory_used_data]
     plot_metrics(metric_names, metric_data, maze_sizes_mdp_algo)
 
@@ -112,14 +116,65 @@ def compare_mdp_algos(maze_sizes_mdp_algo):
     print("\nResults Table:")
     print(f"{'Maze Size':<15}{'Algorithm':<25}{'Time Taken':<15}{'Iterations Needed':<25}{'Memory Used':<15}")
     for size in maze_sizes_mdp_algo:
-        for algo in [MdpValueIteration.value_iteration, MdpPolicyIteration.policy_iteration]:
+        for algo in MDP_ALGOS:
             print(f"{str(size):<15}{algo.__name__:<25}{time_taken_data[algo.__name__].pop(0):<15.5f}"
                 f"{iterations_data[algo.__name__].pop(0):<25}{memory_used_data[algo.__name__].pop(0):<15.2f}")
 
+def compare_all_algos(maze_sizes_all_algo):
+    # Lists to store metrics
+    time_taken_data = {algo.__name__: [] for algo in ALL_ALGOS}
+    # iterations_data = {algo.__name__: [] for algo in MDP_ALGOS}
+    memory_used_data = {algo.__name__: [] for algo in ALL_ALGOS}
+
+    @memory_usage
+    def run_algo(algo, *args):
+        return algo(*args)
+
+    for i in maze_sizes_all_algo:
+        print(f"Maze size: {i}")
+        input_maze_search = maze_generator.prims_algo(w=i[0], h=i[1])
+        input_maze_mdp = mdp_maze_generator.MdpMaze(w=i[0], h=i[1])
+        
+        for algo in ALL_ALGOS:
+            if algo in MDP_ALGOS:
+                maze = copy.deepcopy(input_maze_mdp)
+            else:
+                maze = copy.deepcopy(input_maze_search)
+            start, end = maze.set_start_end_points()
+            if algo in MDP_ALGOS:
+                start.reward = -100
+                end.reward = 100
+                end.set_state('e')
+            args = [maze, i[0], i[1], start, end]
+
+            start_time = time.time()
+            run_result, memory_used = run_algo(algo, *args)
+            # _, __, iterations = run_result
+            time_taken = time.time() - start_time
+            
+            # Store metrics
+            time_taken_data[algo.__name__].append(time_taken)
+            # iterations_data[algo.__name__].append(iterations)
+            memory_used_data[algo.__name__].append(memory_used)
+
+    # metric_names = ['Time Taken for convergence', 'Iterations Needed for convergence', 'Memory Used']
+    metric_names = ['Time Taken', 'Runtime Memory Usage']
+    metric_data = [time_taken_data, memory_used_data]
+    plot_metrics(metric_names, metric_data, maze_sizes_all_algo)
+
+
+    print("\nResults Table:")
+    print(f"{'Maze Size':<15}{'Algorithm':<25}{'Time Taken':<15}{'Memory Used':<15}")
+    for size in maze_sizes_all_algo:
+        for algo in ALL_ALGOS:
+            print(f"{str(size):<15}{algo.__name__:<25}{time_taken_data[algo.__name__].pop(0):<15.5f}"
+                f"{memory_used_data[algo.__name__].pop(0):<15.2f}")
+
 
 if __name__ == '__main__':
-    # maze_sizes_search_algo = [(50, 50), (100, 100), (250, 250), (500, 500), (1000, 1000)]
-    # compare_search_algos(maze_sizes_search_algo)
+    maze_sizes_search_algo = [(50, 50), (100, 100), (250, 250), (500, 500), (1000, 1000)]
+    compare_search_algos(maze_sizes_search_algo)
 
-    maze_sizes_mdp_algo = [(10, 10), (50, 50)]#, (100, 100)]
+    maze_sizes_mdp_algo = [(10, 10), (50, 50), (100, 100)]
     compare_mdp_algos(maze_sizes_mdp_algo)
+    compare_all_algos(maze_sizes_all_algo=maze_sizes_mdp_algo)
--- readme.txt
+++ readme.txt
@@ -1,11 +1,22 @@
-To create a maze and visualize it use:
-python .\maze_generator.py -l 50 -w 150
+Run `pip install -r requirements.txt` to install the requirements for this project.
 
-Command to run DFS algorithm:
-python .\DFS.py -l 50 -w 100 -v Y
+To create a maze of side length 50 and width 50 and visualize it use:
+`python .\maze_generator.py -l 50 -w 150`
 
-Command to run BFS algorithm:
-python .\BFS.py -l 50 -w 100 -v Y
+Command to run algorithms with maze side length specified
+by -l flag and width specified by -w and visualization flag
+-v. Visualization can be switched off by not passing the -v flag
 
-Command to run A* algorithm:
-python .\astar.py -l 50 -w 100 -v Y
+`python .\DFS.py -l 50 -w 100 -v Y`
+
+`python .\BFS.py -l 50 -w 100 -v Y`
+
+`python .\astar.py -l 50 -w 100 -v Y`
+
+`python .\MdpPolicyIteration.py -l 50 -w 50 -v Y `
+
+`python .\MdpValueIteration.py -l 50 -w 50 -v Y `
+
+
+For more help, run:
+`python <algo-filename> --help`
